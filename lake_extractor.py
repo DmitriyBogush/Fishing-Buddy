5403d14e059e448a4971f645a0fbd94c98752881
DmitriyBogush <dmitriyabogush@gmail.com>
2025-03-05 22:18:28 -0800

    Got coordinates of lakes :)

--- lake_extractor.py
+++ lake_extractor.py
@@ -1,5 +1,6 @@
 import re
-import overpy
+import overpass
+import geojson
 from pypdf import PdfReader
 
 
@@ -16,34 +17,54 @@ for i in range(85,86):
 
 # Extract all of the lake names using regex 
 res = re.findall(r'\w*\W*[ ]LAKE\s\w*|(?<=\n)LAKE\s\w*', lakestr)
+lakes = []
 
+# Convert to lover case and remove commas and extra space at end 
 for lake in res:
-    print(lake)
+    lakes.append(lake.lower().replace(',','').title().rstrip())
 
-# Get the coordinates of a lake
-api = overpy.Overpass()
+for lake in lakes:
+  print(lake)
+
+# Get the coordinates of a lake using overpass API 
+api = overpass.API()
 
 # What are we looking for 
-lake = "Banks Lake"
+lakescoord = []
 
-# Query Overpass API 
-query = """<osm-script output="json" output-config="" timeout="25">
-  <union into="_">
-    <query into="_" type="way">
-      <has-kv k="natural" modv="" v="water"/>
-      <has-kv k="name" modv="" v="{lookingFor}"/>
-      <bbox-query s="45.460130637921" w="-125.15625" n="49.095452162535" e="-116.6748046875"/>
-    </query>
-    <query into="_" type="relation">
-      <has-kv k="natural" modv="" v="water"/>
-      <has-kv k="name" modv="" v="{lookingFor}"/>
-      <bbox-query s="45.460130637921" w="-125.15625" n="49.095452162535" e="-116.6748046875"/>
-    </query>
-  </union>
-  <print e="" from="_" geometry="center" ids="yes" limit="" mode="body" n="" order="id" s="" w=""/>
-</osm-script>""".format(lookingFor=lake)
+# Query Overpass API KML Format  
+# query = """<osm-script output="json" output-config="" timeout="25">
+#   <union into="_">
+#     <query into="_" type="way">
+#       <has-kv k="natural" modv="" v="water"/>
+#       <has-kv k="name" modv="" v="{lookingFor}"/>
+#       <bbox-query s="45.460130637921" w="-125.15625" n="49.095452162535" e="-116.6748046875"/>
+#     </query>
+#     <query into="_" type="relation">
+#       <has-kv k="natural" modv="" v="water"/>
+#       <has-kv k="name" modv="" v="{lookingFor}"/>
+#       <bbox-query s="45.460130637921" w="-125.15625" n="49.095452162535" e="-116.6748046875"/>
+#     </query>
+#   </union>
+#   <print e="" from="_" geometry="center" ids="yes" limit="" mode="body" n="" order="id" s="" w=""/>
+# </osm-script>""".format(lookingFor=lake)
 
-coord = api.query(query)
+# Find all lakes 
+for lake in lakes:
+  query = """
+  (
+    way["natural"="water"]["name"="{LookingFor}"](45.460130637921,125.15625,49.095452162535,-116.6748046875);
+    relation["natural"="water"]["name"="{LookingFor}"](45.460130637921,125.15625,49.095452162535,-116.6748046875);
+  );out center;
+  """.format(LookingFor=lake)
 
-# Extract the coordinates 
-print(type(coord))
+  # Query the api 
+  coord = api.get(query, responseformat="csv(name,::lon,::lat)")
+
+  # Extract the coordinates print if we didnt find the lake 
+  if len(coord) == 1:
+    print("Did not find", lake)
+  else:  
+    lakescoord += [coord[1]]
+
+print(lakescoord)
